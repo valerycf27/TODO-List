5764d71932eb783e9bf35c784d4ba825c959d7ff
ahmad-en <64755472+ahmad-en@users.noreply.github.com>
2022-11-15 02:14:44 +0100

    Add files via upload
    Django To-Do List Web Application with many additional security features.

--- base/models.py
+++ base/models.py
@@ -1,19 +1,31 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django import forms
 # Create your models here.
 
 
 class Task(models.Model):
+    # One-to-many relationship.
+    # When a user is deleted, all his items are also deleted.
+    # Nulls are possible.
+    # Forms may be blank.
+    # user = models.ForeignKey(
+    #     User, on_delete=models.CASCADE, null=True, blank=True)
     user = models.ForeignKey(
-        User, on_delete=models.CASCADE, null=True, blank=True)
+    User, on_delete=models.CASCADE, null=True, blank=True)
+    
+    # Field for the title. A title may be empty.
     title = models.CharField(max_length=200)
+    
+    # A text field for the description of the task.
     description = models.TextField(null=True, blank=True)
-    date = models.DateTimeField()
+    
+    # Checkbox to mark an item as completed.
     complete = models.BooleanField(default=False)
+    
+    # Automatically take the date and time at the moment of creation a new item.
     created = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):   
+    def __str__(self):
         return self.title
 
     class Meta:
--- base/urls.py
+++ base/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, DeleteView, CustomLoginView, RegisterPage, TaskReorder
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, DeleteView, CustomLoginView, RegisterPage, TaskReorder, contact
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
+
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('register/', RegisterPage.as_view(), name='register'),
 
@@ -13,4 +14,5 @@ urlpatterns = [
     path('task-update/<int:pk>/', TaskUpdate.as_view(), name='task-update'),
     path('task-delete/<int:pk>/', DeleteView.as_view(), name='task-delete'),
     path('task-reorder/', TaskReorder.as_view(), name='task-reorder'),
+
 ]
